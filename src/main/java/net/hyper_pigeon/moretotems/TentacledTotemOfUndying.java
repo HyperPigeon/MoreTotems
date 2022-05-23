11d895e6a5887620016cfb3ca9430ff056e76622
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2022-05-23 13:19:58 -0400

    updated to 1.18.2

--- src/main/java/net/hyper_pigeon/moretotems/TentacledTotemOfUndying.java
+++ src/main/java/net/hyper_pigeon/moretotems/TentacledTotemOfUndying.java
@@ -1,13 +1,6 @@
 package net.hyper_pigeon.moretotems;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
 
 public class TentacledTotemOfUndying extends Item {
 
